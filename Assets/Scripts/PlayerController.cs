3eb2698c44e519c74fa8a1cb8e064b174305dc81
zinine73 <zinine@naver.com>
2025-12-04 15:48:08 +0900

    UI toolkit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,27 +1,50 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
+    public UIDocument uIDocument;
+    public GameObject explosionEffect;
     public GameObject boosterFlame;
     public float thrustForce = 1f;
     public float maxSpeed = 5f;
     public float scoreMultiplier = 10f;
+
     float score = 0f;
     float elapsedTime = 0f;
     Rigidbody2D rb;
+    Label scoreText;
+    Button restartButton;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        scoreText = uIDocument.rootVisualElement
+            .Q<Label>("ScoreLabel");
+        restartButton = uIDocument.rootVisualElement
+            .Q<Button>("RestartButton");
+        restartButton.style.display = DisplayStyle.None;
+        restartButton.clicked += ReloadScene;
     }
 
     void Update()
     {
+        UpdateScore();
+        MovePlayer();
+        SetBoosterView();
+    }
+
+    void UpdateScore()
+    {
         elapsedTime += Time.deltaTime;
         score = Mathf.FloorToInt(elapsedTime * scoreMultiplier);
-        Debug.Log("Score : " + score);
+        scoreText.text = $" Score : {score} ";
+    }
 
+    void MovePlayer()
+    {
         // 마우스 왼쪽 버튼이 지금 눌렸느냐 판단
         if (Mouse.current.leftButton.isPressed) 
         {
@@ -41,7 +64,10 @@ public class PlayerController : MonoBehaviour
                     rb.linearVelocity.normalized * maxSpeed;
             }
         }
+    }
 
+    void SetBoosterView()
+    {
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
             // 부스터 보이기
@@ -57,5 +83,14 @@ public class PlayerController : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision)
     {
         Destroy(gameObject);
+        Instantiate(explosionEffect, 
+            transform.position, transform.rotation);
+        restartButton.style.display = DisplayStyle.Flex;
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(
+            SceneManager.GetActiveScene().name);
     }
 }
